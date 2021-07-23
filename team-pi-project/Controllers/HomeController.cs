def4b699bc7c68c8e45678521497696919334b56
Caleb-Huss <caleb.huss97@gmail.com>
2021-07-23 10:50:29 -0500

    Removed code smells

--- team-pi-project/Controllers/HomeController.cs
+++ team-pi-project/Controllers/HomeController.cs
@@ -11,11 +11,11 @@ namespace team_pi_project.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController()
         {
-            _logger = logger;
+
         }
 
         public IActionResult Index()
